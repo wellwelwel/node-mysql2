01c8574dc1beda333ed5e2e14d164f40eee95076
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-02-15 09:25:19 -0300

    ci: simplify tests

--- website/test/index.ts
+++ website/test/index.ts
@@ -1,16 +1,6 @@
-import fs from 'node:fs';
-import path from 'node:path';
+import { poku } from 'poku';
 
-const runTests = async (dir: string) => {
-  const testDir = path.join(__dirname, dir);
-  const files = fs.readdirSync(testDir);
-
-  for (const file of files) {
-    if (file.endsWith('.ts')) {
-      await import(path.join(testDir, file));
-      console.log(`✅ ${file}`);
-    }
-  }
-};
-
-runTests('./unit');
+poku('./test/unit', {
+  parallel: true,
+  filter: /\.(ts)$/,
+});
