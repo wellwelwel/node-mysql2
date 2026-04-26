780bc274727e6ec86f2994d60d958b3ce8ee2937
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-04-26 04:53:33 -0300

    chore: debug

--- poku.config.js
+++ poku.config.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+const { coverage } = require('@pokujs/coverage');
 const { multiSuite } = require('@pokujs/multi-suite');
 const { defineConfig, listFiles } = require('poku');
 const { hasPrivileges } = require('./tools/common.js');
@@ -39,5 +40,20 @@ const sequential = defineConfig({
 });
 
 module.exports = defineConfig({
-  plugins: [multiSuite([parallel, sequential])],
+  plugins: [
+    coverage({
+      requireFlag: true,
+      all: true,
+      include: ['index.js', 'promise.js', 'lib/**/*.js'],
+      exclude: ['mysqldata/**', 'node_modules/**', 'test/**'],
+      reporter: ['text', 'lcov', 'v8'],
+      statements: 80,
+      branches: 80,
+      functions: 77,
+      lines: 80,
+      checkCoverage: true,
+      clean: true,
+    }),
+    multiSuite([parallel, sequential]),
+  ],
 });
